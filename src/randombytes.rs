b55c32da45e0f4e831375e78061b64bf3592f01b
Jason Gerard DeRose <jasonderose@gmail.com>
2025-02-09 05:54:45 -0700

    Update to latest getrandom, rand_core, rand

--- src/randombytes.rs
+++ src/randombytes.rs
@@ -1,5 +1,7 @@
+use rand;
 use rand::prelude::*;
 
+// FIXME: This slicing inside x feels weird and risky
 pub fn randombytes(x: &mut [u8], len: usize) {
-  thread_rng().fill_bytes(&mut x[..len])
+  rand::rng().fill_bytes(&mut x[..len])
 }
